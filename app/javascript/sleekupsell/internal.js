39643eb5bde5de98831bd5ee5f729750c97b4f27
Prometheus <berjistechnologies@gmail.com>
2022-03-13 01:22:16 +0300

    override anchor tags

--- app/javascript/sleekupsell/internal.js
+++ app/javascript/sleekupsell/internal.js
@@ -1,4 +1,11 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async () => {
+    $('a').on('click', (e) => {
+        e.preventDefault()
+        e.stopPropagation()
+        let href = $(e.target).attr('href')
+        window.top.location = app_path + href
+    })
+
     console.log(`${controller} ${action}`)
     if (controller === 'offers' && action === 'new') {
         document.querySelector('[action="/offers"]').addEventListener('submit', (e) => {
